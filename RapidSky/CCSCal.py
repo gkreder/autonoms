0270ee5ff3d65ef7e4ce282528e5d6dfb93c014e
gkreder <gkreder@gmail.com>
2023-01-24 09:18:53 +0100

    Mod CCCal

---
+++ RapidSky/CCSCal.py
@@ -0,0 +1,63 @@
+########################################################################
+import sys
+import os
+import argparse
+import deimos
+import pandas as pd
+import numpy as np
+########################################################################
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--inMZML', required = True)
+parser.add_argument('-t', '--tuneIonsFile', required = True)
+parser.add_argument('-o', '--outDir', required = True)
+parser.add_argument('--tol', default = 0.01)
+parser.add_argument('--bufferGasMass', default = 28.006148)
+# 28.006148 - AutoCCS N2
+# 28.013 - Deimos N2
+args = parser.parse_args()
+########################################################################
+
+print('Loading data...')
+data = deimos.load(args.inMZML, accession={'retention_time': 'MS:1000016', 'drift_time': 'MS:1002476', 'Positive Scan' : "MS:1000130"})
+ms1 = data['ms1']
+mode = {1.0 : 'Positive', 0 : 'Negative'}[ms1['Positive Scan'].mode().values[0]]
+modeInt = {'Positive' : 1.0, 'Negative' : -1.0}[mode]
+dfRef = pd.read_csv(args.tuneIonsFile)
+dfRef = pd.DataFrame(dfRef[dfRef['Precursor Charge'] * modeInt >= 1])
+print(f'...done, found {mode} mode data\n\n')
+
+print('Scraping tune ion values...')
+tas = []
+for m in dfRef['Precursor m/z'].values:
+    ss = deimos.locate(ms1, by=['mz'], loc=[m], tol=[args.tol], return_index=False)
+    # slice by mass
+    # l = m - tol
+    # h = m + tol
+    # ss = deimos.slice(ms1, by='mz', low = l, high = h)
+    # t = ms1[(ms1['mz'] - m).abs() <= tol]
+    qs = np.quantile(ss.intensity, q = [0, 0.1, 0.5, 0.7, 0.9])
+    q90 = qs[-1]
+    # ta = ss.loc[lambda x : x['intensity'] > q90].drift_time.mean()
+    ta = np.average(ss['drift_time'], weights = ss['intensity'])
+    print(m, ta)
+    tas.append(ta)
+
+print('...done')
+ccs_cal = deimos.calibration.calibrate_ccs(mz = dfRef['Precursor m/z'], ccs = dfRef['CCS'], q = dfRef['Precursor Charge'], ta = tas, buffer_mass = args.bufferGasMass)
+print(f'r-squared:\t{ccs_cal.fit["r"] ** 2}')
+print(f"Beta - {ccs_cal.beta}")
+print(f"TFix - {ccs_cal.tfix}")
+
+
+override_string = f'''<?xml version="1.0" encoding="utf-8"?>
+<OverrideImsCalibration>
+    <FileVersion>1</FileVersion>
+    <SingleFieldCcsCalibration>
+        <DriftGas mass="{args.bufferGasMass}">N2</DriftGas>
+        <TFix>{ccs_cal.tfix}</TFix>
+        <Beta>{ccs_cal.beta}</Beta>
+    </SingleFieldCcsCalibration>
+</OverrideImsCalibration>'''
+
+with open(os.path.join(args.outDir, 'OverrideImsCal.xml'), 'w') as f:
+    print(override_string, file = f)
---
+++ preprocess.nf
@@ -0,0 +1,88 @@
+#!/usr/bin/env nextflow
+
+/*
+vim: syntax=groovy
+-*- mode: groovy;-*-
+ *
+ * Copyright (c) 2013-2016, Centre for Genomic Regulation (CRG).
+ * Copyright (c) 2013-2016, Paolo Di Tommaso and the respective authors.
+ *
+ *   This file is part of 'Nextflow'.
+ *
+ *   Nextflow is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Nextflow is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Nextflow.  If not, see <http://www.gnu.org/licenses/>.
+ */ 
+ 
+/* 
+ * 
+ * 
+ * Try to run this example by entering the following command line: 
+ * 
+ *   nextflow run multiple_inputs.nf --query data/prot_\*.fa
+ * 
+ * It will execute a blast search for each file that matches the 
+ * the wildcard path matcher 
+ * 
+ */
+ 
+/* 
+ * Define the pipeline default parameters. 
+ */  
+// params.query = "$baseDir/*.d"
+params.query = "/Users/reder/OneDrive/convex-landing/sample_d_files/*.d"
+// params.db = "$baseDir/blast-db/pdb/tiny"
+// params.out = 'blast_result.txt'
+
+/* 
+ * the path where the BLAST DB is located 
+ */
+ 
+// db_name = file(params.db).name
+// db_path = file(params.db).parent
+
+/* 
+ * A channel emitting the fasta files specified as parameter
+ */
+seq = Channel.fromPath(params.query)
+
+/*
+ * Execute a BLAST job for each chunk for the provided sequences
+ */
+
+process blast {
+    tag { x.baseName }
+
+    input:
+    file x from seq
+    // file db_path
+
+    // output:
+    // file 'out' into blast_result
+
+    """
+    # blastp -db $db_path/$db_name -query $x -outfmt 6 > out
+    echo hello world
+    echo $x
+    """
+}
+
+/* 
+ * Collect all the outputs produced by the `blast` process
+ * executions to a single file, whose name is defined by  
+ * the `params.out` parameters
+ */
+ 
+//  blast_result.println("hello world 2")
+// blast_result
+//   .collectFile(name: file(params.out))
+//   .println { "Result saved to file: $it" }
